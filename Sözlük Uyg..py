eaa33953addb9a23ae3ec42b3a6b52f5429f154b
Can <58526508+canustun@users.noreply.github.com>
2022-11-19 22:55:27 +0300

    Add files via upload

---
+++ Sözlük Uyg..py
@@ -0,0 +1,27 @@
+#Türkçe sözlük
+import requests as req
+from bs4 import BeautifulSoup
+import os
+
+
+while True:
+    os.system("cls||clear")
+    kelime=input("********************\nKelimeyi giriniz :")
+    kelime_ara=req.get(f"http://www.birsozluk.com/{kelime}")
+    if len(kelime)<=1:
+        input("Daha uzun bi kelime girin! Tekrar denemek için boşluk tuşuna basınız!")
+        continue
+
+    kaynak = BeautifulSoup(kelime_ara.content,"html.parser")
+    span = kaynak.find("table")
+    sonuc = list(span.stripped_strings)
+    
+    if sonuc[3] == "Bilgi yarışması":
+        print("\nAradığınız kelime kayıtlarımızda mevcut değil yada hatalı bir kelime girdiniz!")
+    else:
+        print(f"""\n************* Kelime anlamı *************\n
+{"-"*40}\nArama yapılan kelime : {kelime}
+Anlam : {sonuc[3][:-1]}\n{"-"*40}
+""")
+        
+    input("\nYeni kelime aramak için boşluk tuşuna basın...")
